cd817cb70a42baaa746be95e28d7120f6a356033
Ryan Harings <ryanharings@hotmail.com>
2016-09-13 11:18:45 -0600

    finished up to functionality

---
+++ src/server/db/migrations/20160913095309_users.js
@@ -0,0 +1,10 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', function(table) {
+    table.increments();
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('users');
+};
